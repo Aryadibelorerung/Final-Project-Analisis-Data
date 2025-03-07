fa544b4b5c2225c72ba5f1161bdbabdc53e99e24
Aryadi Belo Rerung <aryadibelorerung30@gmail.com>
2025-03-07 20:45:05 +0800

    initial commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import babel
 from babel.numbers import format_currency
 
 # Mengatur tema seaborn
